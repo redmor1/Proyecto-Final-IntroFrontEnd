ddd72cc372992b6df8cd7a0e667037a0f76782b1
redmor1 <lightz17@hotmail.com>
2023-01-22 17:55:01 -0300

    Agregar placeholders para nombres e informacion

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 'use strict'
 
 
-// hacer REQUEST a la api de randomuser.me y usar la RESPONSE para renderizar una persona
+//hacer REQUEST a la api de randomuser.me y usar la RESPONSE para renderizar una persona
 fetch('https://randomuser.me/api')
     .then(response => response.json())
     .then(response => renderPersona(response.results[0]))
 
-// cambiar elementos HTML usando el parametro
+//cambiar elementos HTML usando el parametro
 function renderPersona(persona) {
 
     document.getElementById('fotoPerfil').src = persona.picture.large ;
@@ -16,8 +16,20 @@ function renderPersona(persona) {
     document.getElementById('email').innerHTML = persona.email;
     document.getElementById('telefono').innerHTML = persona.phone;
     document.getElementById('direccion').innerHTML = `${persona.location.street.number} ${persona.location.street.name}`;
+    
+    //Conseguir todos los elementos con la clase "placeholder" y borrarlos
+    let elementosPlaceholder = document.querySelectorAll('.placeholder');
+        for (let i = 0; i < elementosPlaceholder.length; i++) {
+            elementosPlaceholder[i].remove()
+        }
+    };
 
-}
+    //Conseguir todos los elementos con la clase "placeholder-wave" y removerles esa clase
+    let elementosPlaceholderWave = document.querySelectorAll(".placeholder-wave");
+        for (let i = 0; i < elementosPlaceholderWave.length; i++)
+        elementosPlaceholderWave[i].classList.remove("placeholder-wave");
+    ;
+
 
 
 // TODO: hacer lista random de titulos
